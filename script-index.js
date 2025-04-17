e20003c3ec8576812767922446c6c2f07ea2ed99
angierincont <angie.rincont@uniagustiniana.edu.co>
2025-04-17 21:25:12 +0000

    5

---
+++ script-index.js
@@ -0,0 +1,5 @@
+document.getElementById("loginBtn").addEventListener("click", function() {
+    alert("¡Has iniciado sesión correctamente! (Simulación)");
+    // Aquí podrías redirigir a la página de estaciones o cualquier otra funcionalidad que quieras agregar
+  });
+  
